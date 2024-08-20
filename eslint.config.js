40267d4da99cb7f269ca8707438ee1046433a9ce
ruanzhixiong <ruanzhixiong@bobandata.com>
2024-08-20 15:34:07 +0800

    fix(eslint-config): 修复 ESLint 配置中的语法错误

--- eslint.config.js
+++ eslint.config.js
@@ -5,29 +5,42 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
-export default tseslint.config({
-  extends: [
-    js.configs.recommended,
-    ...tseslint.configs.recommended,
-    eslintPluginPrettierRecommended,
-  ],
-  files: ["**/*.{ts,tsx}"], // eslint 检测的文件，根据需要自行设置
-  ignores: ["dist", "node_modules"],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
-  },
-  plugins: {
-    "react-hooks": reactHooks,
-    "react-refresh": reactRefresh,
-  },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
+// https://www.cnblogs.com/jsonq/p/18357943
+export default [
+  { ignores: ["**/dist/**", "**/node_modules/**", ".history", "public/"] },
+  js.configs.recommended, // eslint-config-prettier
+  ...tseslint.configs.recommended, // eslint-config-prettier
+  eslintPluginPrettierRecommended,
+  {
 
-    "prettier/prettier": "warn",
-    "arrow-body-style": "off",
-    "prefer-arrow-callback": "off",
-
-    "@typescript-eslint/no-explicit-any": "warn", // allow any type
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    }
   },
-});
+  {
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+
+      "prettier/prettier": "warn",
+      "arrow-body-style": "off",
+      "prefer-arrow-callback": "off",
+
+      '@typescript-eslint/no-this-alias': 'off',
+      '@typescript-eslint/ban-ts-ignore': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-unsafe-function-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-use-before-define': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/ban-types': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+    },
+  }
+]
